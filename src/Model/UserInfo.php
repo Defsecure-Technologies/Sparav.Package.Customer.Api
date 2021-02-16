d76335f91d5f901a8b2c0b6a99a8a1d22ad99a63
blerimcazimi <blerim@tandbud.dk>
2021-02-16 10:49:16 +0100

    UserInfo allow null

--- src/Model/UserInfo.php
+++ src/Model/UserInfo.php
@@ -7,28 +7,28 @@ namespace Sparav\Customer\Model;
 class UserInfo
 {
     public int $customer_id;
-    public string $firstname;
-    public string $lastname;
-    public string $email;
-    public string $prefix;
-    public string $phone;
-    public string $address;
-    public string $zipcode;
-    public string $country;
+    public ?string $firstname;
+    public ?string $lastname;
+    public ?string $email;
+    public ?string $prefix;
+    public ?string $phone;
+    public ?string $address;
+    public ?string $zipcode;
+    public ?string $country;
 
     /**
      * UserInfo constructor.
      * @param int $customer_id
-     * @param string $firstname
-     * @param string $lastname
-     * @param string $email
-     * @param string $prefix
-     * @param string $phone
-     * @param string $address
-     * @param string $zipcode
-     * @param string $country
+     * @param string|null $firstname
+     * @param string|null $lastname
+     * @param string|null $email
+     * @param string|null $prefix
+     * @param string|null $phone
+     * @param string|null $address
+     * @param string|null $zipcode
+     * @param string|null $country
      */
-    public function __construct(int $customer_id, string $firstname, string $lastname, string $email, string $prefix, string $phone, string $address, string $zipcode, string $country)
+    public function __construct(int $customer_id, ?string $firstname, ?string $lastname, ?string $email, ?string $prefix, ?string $phone, ?string $address, ?string $zipcode, ?string $country)
     {
         $this->customer_id = $customer_id;
         $this->firstname = $firstname;
